6917a5ee52f5fd235486105e300e1fb625eea7bf
Marcin Grygierczyk <kwaska1000@gmail.com>
2020-01-06 10:16:23 +0100

    init

---
+++ Assets/WorkingDir/Scripts/Ball.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public GameObject plane;
+    public GameObject spawnPoint;
+    public GameObject gameOverImage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < plane.transform.position.y - 10)
+        {
+            Debug.Log("position y: " + transform.position.y);
+            transform.position = spawnPoint.transform.position;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Finish"))
+            GameOver();
+    }
+
+    void GameOver()
+    {
+        gameOverImage.SetActive(true);
+    }
+}
---
+++ Assets/WorkingDir/Scripts/virtual_btn.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+using Vuforia;
+
+public class virtual_btn : MonoBehaviour, IVirtualButtonEventHandler
+{
+    public GameObject virtualButton;
+    public VideoPlayer videoPlayer;
+
+    void Start()
+    {
+        virtualButton.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        Debug.LogWarning("Pressed");
+        videoPlayer.Play();
+        Debug.LogWarning("asdf");
+
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        Debug.LogWarning("Released");
+    }
+}
