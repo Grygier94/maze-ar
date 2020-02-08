3db6c440722333dd92a222e336af4eb60eaac926
Marcin Grygierczyk <kwaska1000@gmail.com>
2020-02-09 00:07:30 +0100

    Handle OnTrackingFound event

---
+++ Assets/WorkingDir/Scripts/VuforiaEventHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VuforiaEventHandler : DefaultTrackableEventHandler
+{
+    protected override void OnTrackingFound()
+    {
+        if (mTrackableBehaviour)
+        {
+            Debug.Log("NAKE: ");
+            Debug.Log(mTrackableBehaviour.TrackableName);
+            switch (mTrackableBehaviour.TrackableName)
+            {
+                case "Maze0":
+                    SceneManager.LoadScene("Level_1");
+                    break;
+                case "Maze1":
+                    SceneManager.LoadScene("Level_2");
+                    break;
+                case "Maze2":
+                    SceneManager.LoadScene("Level_3");
+                    break;
+            }
+        }
+    }
+}
