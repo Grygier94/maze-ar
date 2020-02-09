fd9b3f78ff528656a0a08ede88377a1ba282b8b8
Marcin Grygierczyk <kwaska1000@gmail.com>
2020-02-09 09:53:10 +0100

    Remove debug logs

--- Assets/WorkingDir/Scripts/VuforiaEventHandler.cs
+++ Assets/WorkingDir/Scripts/VuforiaEventHandler.cs
@@ -9,8 +9,6 @@ public class VuforiaEventHandler : DefaultTrackableEventHandler
     {
         if (mTrackableBehaviour)
         {
-            Debug.Log("NAKE: ");
-            Debug.Log(mTrackableBehaviour.TrackableName);
             switch (mTrackableBehaviour.TrackableName)
             {
                 case "Maze0":
